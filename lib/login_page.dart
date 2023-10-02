502b097413e7ee3c70d92577e5ff4906bcbbf990
Juan Andres Porras <juana.porras@upb.edu.co>
2023-10-02 13:06:56 -0500

    feat: login page

---
+++ lib/login_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class login_page extends StatelessWidget {
+  const login_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("Something New...")
+    );
+  }
+}
