81b992f07c88dfea573417be7e9a3924951604ee
Juan Andres Porras <juana.porras@upb.edu.co>
2023-10-02 18:53:06 -0500

    feat: Basic login page structure

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -5,9 +5,32 @@ class login_page extends StatelessWidget {
   const login_page({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Something New...")
+Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 128, 0, 110),
+      body: Column(
+        children: [
+          const Spacer(),
+          const Padding(
+            padding:  EdgeInsets.all(50),
+            child:  Text("Something New... \n Something Different... lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud",
+            textAlign:  TextAlign.start,
+            style: TextStyle(
+              fontSize: 30,
+            ),
+            ),
+          ),
+          const Spacer(),
+          TextFormField(),
+          const SizedBox(
+            height: 60,
+            width: 120,
+            child:  InkWell(),
+            
+          ),
+          const Spacer(),
+        ],
+      ),
     );
   }
 }
