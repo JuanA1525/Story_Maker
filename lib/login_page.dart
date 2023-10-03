d8476ff255e68656a034eeba748ce6c3902da62a
Juan Andres Porras <juana.porras@upb.edu.co>
2023-10-02 19:23:33 -0500

    feat: Mayor login page upgrade

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 // ignore: camel_case_types
@@ -7,25 +9,59 @@ class login_page extends StatelessWidget {
   @override
 Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 128, 0, 110),
+      backgroundColor: const Color(0xff141414),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const Spacer(),
           const Padding(
-            padding:  EdgeInsets.all(50),
-            child:  Text("Something New... \n Something Different... lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud",
-            textAlign:  TextAlign.start,
+            padding:  EdgeInsets.fromLTRB(50, 15, 50, 15),
+            child:  Text("Welcome to Story Maker",
+            textAlign:  TextAlign.center,
             style: TextStyle(
               fontSize: 30,
+              color: Color(0xffFFFFFF)
             ),
             ),
           ),
           const Spacer(),
-          TextFormField(),
-          const SizedBox(
-            height: 60,
-            width: 120,
-            child:  InkWell(),
+          Padding(
+            padding: const EdgeInsets.all(50),
+            child: TextFormField(
+              style: const TextStyle(color: Color(0xffFFFFFF)),
+              decoration: const InputDecoration(
+                labelText: "Access Code...",
+                labelStyle: TextStyle(color: Color(0xffFFFFFF)),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color.fromARGB(255, 13, 141, 53)),
+                  borderRadius: BorderRadius.all(Radius.circular(25))
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color.fromARGB(255, 3, 67, 23))
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+            width: 150,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shadowColor: const Color(0xffFFFFFF),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(25))
+                ),
+                backgroundColor: const Color(0xff0d8d35)
+                ),
+                child: const Text(
+                  "Ingresar",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Color(0xffFFFFFF)
+                  )
+              ),
+            )
             
           ),
           const Spacer(),
